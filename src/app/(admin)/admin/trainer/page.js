3fa4c9b6fa77351a46d6452223e76117a1bbd416
Qasim-memon <155803590+Qasim-memon@users.noreply.github.com>
2024-10-23 09:59:37 +0500

    done

---
+++ src/app/(admin)/admin/trainer/page.js
@@ -0,0 +1,7 @@
+export default function Trainer() {
+    return (
+      <div className="min-h-screen p-10">
+        <h1 className="text-4xl font-bold text-center">Trainer</h1>
+      </div>
+    );
+  }
